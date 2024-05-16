07d772e0dc4c788032c51be8bc9f164aa469fcdb
vitalii.martyniak <vitaliy7martynyak14@gmail.com>
2024-05-17 01:11:42 +0200

    Initialised skills and services page

--- src/app/views/skills-and-services/skills-and-services.component.ts
+++ src/app/views/skills-and-services/skills-and-services.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-skills-and-services',
   standalone: true,
-  imports: [],
+    imports: [
+        RouterLink
+    ],
   templateUrl: './skills-and-services.component.html',
   styleUrl: './skills-and-services.component.scss'
 })
