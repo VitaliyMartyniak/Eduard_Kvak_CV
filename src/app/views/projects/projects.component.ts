111b62bf0bc54606bc520ea312571a1d84bfc518
vitalii.martyniak <vitaliy7martynyak14@gmail.com>
2024-05-21 10:53:25 +0200

    Projects page init

--- src/app/views/projects/projects.component.ts
+++ src/app/views/projects/projects.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import {ExpandableSectionComponent} from "../skills-and-services/components/expandable-section/expandable-section.component";
+import {NgForOf} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+    imports: [
+        ExpandableSectionComponent,
+        NgForOf,
+        RouterLink
+    ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
-
+  projects = [
+    {
+      img: 'img',
+      title: 'Car IQ Pay',
+      hashes: ['Automotive', 'Financial services', 'FinTech', 'Fleet management', 'Payments']
+    }
+  ]
 }
