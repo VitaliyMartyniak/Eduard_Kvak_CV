c7f965574ba1b88309005b7ca3e9a41cc5bc3df5
vitalii.martyniak <vitaliy7martynyak14@gmail.com>
2024-05-22 09:56:35 +0200

    Added projects page

--- src/app/views/projects/projects.component.ts
+++ src/app/views/projects/projects.component.ts
@@ -1,25 +1,53 @@
 import { Component } from '@angular/core';
 import {ExpandableSectionComponent} from "../skills-and-services/components/expandable-section/expandable-section.component";
-import {NgForOf} from "@angular/common";
-import {RouterLink} from "@angular/router";
+import {NgForOf, NgStyle} from "@angular/common";
+import {Router, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-    imports: [
-        ExpandableSectionComponent,
-        NgForOf,
-        RouterLink
-    ],
+  imports: [
+    ExpandableSectionComponent,
+    NgForOf,
+    RouterLink,
+    NgStyle
+  ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+
+  constructor(private router: Router) {
+  }
+
   projects = [
     {
-      img: 'img',
+      name: 'car-iq-pay',
       title: 'Car IQ Pay',
-      hashes: ['Automotive', 'Financial services', 'FinTech', 'Fleet management', 'Payments']
+      hashes: ['Automotive', 'Financial services', 'FinTech', 'Fleet management', 'Payments'],
+    },
+    {
+      name: 'clarity',
+      title: 'Clarity',
+      hashes: ['Consulting', 'CRM', 'Data integration', 'E-Commerce', 'Development'],
+    },
+    {
+      name: 'intelas',
+      title: 'Intelas',
+      hashes: ['Real estate', 'FinTech', 'Data integration'],
+    },
+    {
+      name: 'alifBee-kids',
+      title: 'AlifBee Kids',
+      hashes: ['Learing platform'],
     }
   ]
+
+  getBackgroundImageUrl(name: string) {
+    return `url('assets/images/${name}.png')`
+  }
+
+  navigateToProject(name: string) {
+    this.router.navigate([`/${name}`]);
+  }
 }
