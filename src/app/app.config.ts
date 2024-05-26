62d7dd2ddae595852ba2d140574c6a0d9dbd4fa5
vitalii.martyniak <vitaliy7martynyak14@gmail.com>
2024-05-26 20:40:02 +0200

    Added animations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import {provideRouter, withInMemoryScrolling} from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideAnimations} from "@angular/platform-browser/animations";
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -9,5 +10,6 @@ export const appConfig: ApplicationConfig = {
       withInMemoryScrolling({
         scrollPositionRestoration: "top",
       })),
+    provideAnimations()
   ]
 };
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,37 +1,44 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {animate, state, style, transition, trigger} from "@angular/animations";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     RouterLink,
+    NgForOf,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  // animations: [
-  //   trigger('shrinkAnimation', [
-  //     state('cornered', style({
-  //       borderRadius: '0px',
-  //     })),
-  //     state('rounded', style({
-  //       borderRadius: '10px'
-  //     })),
-  //     transition('cornered <=> rounded', [
-  //       animate('1s ease-out')
-  //     ]),
-  //   ]),
-  // ],
+  animations: [
+    trigger('borderRadiusAnimation', [
+      state('cornered', style({
+        borderRadius: '0px',
+      })),
+      state('rounded', style({
+        borderRadius: '56px'
+      })),
+      transition('cornered <=> rounded', [
+        animate('1s ease-out')
+      ]),
+    ]),
+  ],
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit, AfterViewInit{
   borderState = 'cornered'
+  mainText = 'Hi, I\'m Ed, a seasoned product designer with over 6 years of experience. I craft innovative digital experiences that blend user needs with business goals. Let\'s collaborate to turn your vision into reality.';
+  textArray: string[] = [];
 
   constructor() {
   }
 
   ngOnInit() {
-    this.borderState = 'rounded';
+    this.textArray = this.mainText.split('');
   }
 
+  ngAfterViewInit() {
+    this.borderState = 'rounded';
+  }
 }
